54fc44467d6e29f74d9be00d77514589b2e26ccc
“alioshr” <alioshmpb@gmail.com>
2021-10-12 05:06:36 -0300

    feat: add index.tsx main file

---
+++ src/main/index.tsx
@@ -0,0 +1,8 @@
+import Router from '@/presentation/router/router'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <Router />,
+  document.getElementById('main')
+)
