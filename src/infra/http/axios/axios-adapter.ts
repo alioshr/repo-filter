b7d91c17a7cffe3205e963ea7f35458779b66a92
“alioshr” <alioshmpb@gmail.com>
2021-10-12 04:04:40 -0300

    feat: ensure to return the correct data on success

--- src/infra/http/axios/axios-adapter.ts
+++ src/infra/http/axios/axios-adapter.ts
@@ -6,13 +6,16 @@ implements HttpClient {
   async request (
     data: HttpRequest<RequestBody>
   ): Promise<HttpResponse<ResponseBody>> {
-    await axios.request({
+    const response = await axios.request({
       url: data.url,
       method: data.method,
       headers: data.headers,
       data: data.body,
       params: data.params
     })
-    return await Promise.resolve(null) as any
+    return {
+      statusCode: response.status,
+      body: response.data
+    }
   }
 }
