aebef93c805826617726611704ae270d2fbada59
“alioshr” <alioshmpb@gmail.com>
2021-10-12 00:42:40 -0300

    refactor: turn some query params optional

--- src/domain/usecases/get-repositories.ts
+++ src/domain/usecases/get-repositories.ts
@@ -1,14 +1,14 @@
 import { Paginator, Repository } from '@/domain/models'
 
 export type QueryParamsDTO = {
-  description: string
-  name: string
-  pushed: Date
-  created: Date
-  user: string
-  stars: number
   per_page: number
   page: number
+  description?: string
+  name?: string
+  pushed?: Date
+  created?: Date
+  user?: string
+  stars?: number
 }
 
 export interface GetRepositories {
