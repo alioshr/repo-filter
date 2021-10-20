346088e901e75b967c32c1873ed640a4afdc5799
“alioshr” <alioshmpb@gmail.com>
2021-10-20 01:13:32 -0300

    refactor: reduce usecase to one query and make it required

--- src/domain/tests/mocks/get-repository-mock.ts
+++ src/domain/tests/mocks/get-repository-mock.ts
@@ -4,10 +4,5 @@ import faker from 'faker'
 export const mockedQueryParamsDTO = (): QueryParamsDTO => ({
   page: faker.datatype.number(),
   per_page: faker.datatype.number(),
-  created: faker.date.recent(),
-  pushed: faker.date.recent(),
-  description: faker.random.words(5),
-  name: faker.random.word(),
-  user: faker.random.word(),
-  stars: faker.datatype.number()
+  name: faker.random.word()
 })
--- src/domain/usecases/get-repositories.ts
+++ src/domain/usecases/get-repositories.ts
@@ -3,12 +3,7 @@ import { Paginator, Repository } from '@/domain/models'
 export type QueryParamsDTO = {
   per_page: number
   page: number
-  description?: string
-  name?: string
-  pushed?: Date
-  created?: Date
-  user?: string
-  stars?: number
+  name: string
 }
 
 export interface GetRepositories {
