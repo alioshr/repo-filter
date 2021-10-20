21a4ffd42543400ef1080d0520b6b6f1aa38f8cf
“alioshr” <alioshmpb@gmail.com>
2021-10-20 12:01:51 -0300

    refactor: apply decorator on usecase composition

--- src/main/factories/usecases/get-repositories/get-repositories-factory.ts
+++ src/main/factories/usecases/get-repositories/get-repositories-factory.ts
@@ -1,8 +1,11 @@
 import { RemoteGetRepositories } from '@/data/usecases/get-repositories/remote-get-repositories'
 import { GetRepositories } from '@/domain/usecases/get-repositories'
-import { HttpHeadersDecorator } from '@/main/decorators/http-headers-decorator'
+import { HttpGetRepositoriesListDecorator } from '@/main/decorators/http-get-repositories-list-decorator'
 import { makeAxiosHttpClient } from '../../http/axios-client-factory'
 
 export const makeRemoteGetRepositories = (): GetRepositories => {
-  return new RemoteGetRepositories('https://api.github.com/search/repositories', new HttpHeadersDecorator(makeAxiosHttpClient()))
+  return new RemoteGetRepositories(
+    'https://api.github.com/search/repositories',
+    new HttpGetRepositoriesListDecorator(makeAxiosHttpClient())
+  )
 }
