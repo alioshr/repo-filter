9f04bf6f2885ebbf7c917619625a2e3907a607f5
“alioshr” <alioshmpb@gmail.com>
2021-10-20 13:53:32 -0300

    test:  add noContent validation to factory

--- src/main/factories/pages/repositories-list-validation-factory.test.ts
+++ src/main/factories/pages/repositories-list-validation-factory.test.ts
@@ -7,7 +7,8 @@ describe('RepositoriesListValidationFactory', () => {
   test('ensure Validator Composite is called with the correct validations', () => {
     makeRepositoriesListValidations()
     expect(ValidatorComposite.build).toHaveBeenCalledWith([
-      ...ValidationBuilder.fieldName('name').required().build()
+      ...ValidationBuilder.fieldName('name').required().build(),
+      ...ValidationBuilder.fieldName('items').noContent().build()
     ])
   })
 })
