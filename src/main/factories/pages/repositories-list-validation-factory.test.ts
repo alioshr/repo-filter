d7684b402dadd868324f7568a2a230e173dd4cd8
“alioshr” <alioshmpb@gmail.com>
2021-10-20 12:03:20 -0300

    test: add repositories list validation

---
+++ src/main/factories/pages/repositories-list-validation-factory.test.ts
@@ -0,0 +1,13 @@
+import { ValidationBuilder, ValidatorComposite } from '@/validation/validators'
+import { makeRepositoriesListValidations } from './repositories-list-validation-factory'
+
+jest.mock('@/validation/validators/composite/validator-composite')
+
+describe('RepositoriesListValidationFactory', () => {
+  test('ensure Validator Composite is called with the correct validations', () => {
+    makeRepositoriesListValidations()
+    expect(ValidatorComposite.build).toHaveBeenCalledWith([
+      ...ValidationBuilder.fieldName('name').required().build()
+    ])
+  })
+})
