1a0df87284c6e358c3d03fb6bab45d69c63f03fc
“alioshr” <alioshmpb@gmail.com>
2021-10-19 23:13:20 -0300

    refactor: adjusting coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-    '!**/*.d.ts'
+    '!**/*.d.ts',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/presentation/router/**/*'
   ],
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: [
