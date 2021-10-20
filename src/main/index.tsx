aafb9bdbb752badb356dcf54fb929c9c66c2d17e
“alioshr” <alioshmpb@gmail.com>
2021-10-20 12:04:36 -0300

    refactor: inject composition on root

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,9 @@
 import Router from '@/presentation/router/router'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { makeRepositoriesList } from '@/main/factories/pages/repositories-list-factory'
 
 ReactDOM.render(
-  <Router />,
+  <Router makeRepositoriesList={makeRepositoriesList}/>,
   document.getElementById('main')
 )
