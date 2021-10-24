a8ba3ed26160dba612849d82a4642f614220af14
“alioshr” <alioshmpb@gmail.com>
2021-10-23 21:18:38 -0300

    feat: add final useTruncate hook

--- src/presentation/hooks/use-truncate.ts
+++ src/presentation/hooks/use-truncate.ts
@@ -1,19 +1,23 @@
 export const useTruncate = (text: string, charLimit: number): string => {
-  if (text?.length > charLimit) {
-    const words = text.split(' ')
-    const emptySpaces = words.length - 1
-    let charCount = emptySpaces
-    const stringBatch = []
-    for (let i = 0; i < words.length; i++) {
-      const word = words[i]
-      if (charCount + word.length <= charLimit) {
-        stringBatch.push(word)
-        charCount += word.length
-      } else {
-        break
-      }
-    }
-    return stringBatch.join(' ').concat('...')
+  if (text?.length < charLimit || !text) { return text }
+
+  const words = text.split(' ')
+  const stringBatch = []
+  let charCount = 0
+
+  if (words[0].length > charLimit) {
+    return words[0].slice(0, charLimit).concat('...')
   }
-  return text
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i]
+
+    if (charCount + word.length <= charLimit) {
+      stringBatch.push(word)
+      charCount += word.length + 1
+    } else {
+      break
+    }
+  }
+  return stringBatch.join(' ').concat('...')
 }
